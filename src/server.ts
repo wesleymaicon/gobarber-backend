b17753acbe06ac2f28618be63ed2e0db0c54a5bd
wesleymaicon <wesleymaicon002@gmail.com>
2020-07-29 15:43:52 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+//  app.get('/', (request, response) => response.json({ message: 'Hello world' }));
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello world' });
+});
+
+app.listen(3333, () => {
+    console.log('Server executando na porta 3333!');
+});
